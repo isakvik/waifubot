f965c5d3934c0891957e03953f683a393d4f4ae2
isakvik <hjeisa14@gmail.com>
2017-10-29 03:26:27 +0100

    updated ignores

--- src/main/java/hjeisa/waifubot/Config.java
+++ src/main/java/hjeisa/waifubot/Config.java
@@ -5,9 +5,9 @@ public class Config {
     public static final long min_posting_interval = 60; // seconds
 
     public static final String client_id = "350432098315927574";
-    public static final String client_secret = "2SsnY7jEdGv3rmJa1TEcEwNI-SYZEVxs";
+    public static final String client_secret = "VLzWhn5YpH-Znb_n4juJayWTmvLNiFYb";
 
     public static final String bot_name = "waifubot";
     public static final String bot_ident = "#4906";
-    public static final String bot_token = "MzUwNDMyMDk4MzE1OTI3NTc0.DNbJVA.VcflhvVXD2VBlhywHD6IT5SDiUI";
+    public static final String bot_token = "MzUwNDMyMDk4MzE1OTI3NTc0.DNbMrw.1RfSOkAqmey6coTJ5vcf9ydIBxk";
 }
