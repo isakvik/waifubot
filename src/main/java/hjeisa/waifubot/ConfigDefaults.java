da6d93f2c7cac6e71c4cacb669842591f4a49952
isakvik <hjeisa14@gmail.com>
2017-10-29 03:21:18 +0100

    oops

--- src/main/java/hjeisa/waifubot/ConfigDefaults.java
+++ src/main/java/hjeisa/waifubot/ConfigDefaults.java
@@ -1,13 +1,13 @@
 package hjeisa.waifubot;
 
-public class Config {
+public class ConfigDefaults {
     public static final boolean debug = true;
     public static final long min_posting_interval = 60; // seconds
 
-    public static final String client_id = "350432098315927574";
-    public static final String client_secret = "2SsnY7jEdGv3rmJa1TEcEwNI-SYZEVxs";
+    public static final String client_id = "";
+    public static final String client_secret = "";
 
     public static final String bot_name = "waifubot";
     public static final String bot_ident = "#4906";
-    public static final String bot_token = "MzUwNDMyMDk4MzE1OTI3NTc0.DNbJVA.VcflhvVXD2VBlhywHD6IT5SDiUI";
+    public static final String bot_token = "";
 }
