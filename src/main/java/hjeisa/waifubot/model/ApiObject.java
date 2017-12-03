1a047aa23c388414c2775d239524a5b193adffb5
isakvik <hjeisa14@gmail.com>
2017-12-03 05:43:00 +0100

    created value object for imageboard APIs

--- src/main/java/hjeisa/waifubot/model/ApiObject.java
+++ src/main/java/hjeisa/waifubot/model/ApiObject.java
@@ -1,4 +1,47 @@
 package hjeisa.waifubot.model;
 
-public class APIObject {
+public class ApiObject {
+
+    private String name;
+    private String apiUrl;
+    private String postUrl;
+    private int tagLimit;
+
+    // only danbooru uses this
+    private String tagUrl;
+
+    public ApiObject(String name, String apiUrl, String postUrl, int tagLimit){
+        this.name = name;
+        this.apiUrl = apiUrl;
+        this.postUrl = postUrl;
+        this.tagUrl = null;
+        this.tagLimit = tagLimit;
+    }
+    public ApiObject(String name, String apiUrl, String postUrl, String tagUrl, int tagLimit){
+        this.name = name;
+        this.apiUrl = apiUrl;
+        this.postUrl = postUrl;
+        this.tagUrl = tagUrl;
+        this.tagLimit = tagLimit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getApiUrl() {
+        return apiUrl;
+    }
+
+    public String getPostUrl() {
+        return postUrl;
+    }
+
+    public String getTagUrl() {
+        return tagUrl;
+    }
+
+    public int getTagLimit() {
+        return tagLimit;
+    }
 }
